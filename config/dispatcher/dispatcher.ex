6cef8d6fdf7b7320b4dd68edb52ba7ebd037812c
Rich <piemonkey@protonmail.ch>
2023-08-08 19:02:43 +0200

    Add very basic 'about' endpoint as health check

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -9,15 +9,9 @@ defmodule Dispatcher do
   @json %{ accept: %{ json: true } }
   @html %{ accept: %{ html: true } }
 
-  # In order to forward the 'themes' resource to the
-  # resource service, use the following forward rule:
-  #
-  # match "/themes/*path", @json do
-  #   Proxy.forward conn, path, "http://resource/themes/"
-  # end
-  #
-  # Run `docker-compose restart dispatcher` after updating
-  # this file.
+  match "/about/*path", @json do
+    Proxy.forward conn, path, "http://resource/about/"
+  end
 
   match "/*_", %{ last_call: true } do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
