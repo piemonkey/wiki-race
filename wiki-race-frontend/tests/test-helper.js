5c27e22ea1dc6cafb4b322df0a73d916b3e4d2fd
Rich <piemonkey@protonmail.ch>
2023-08-10 13:13:10 +0200

    Add ember generated frontend

---
+++ wiki-race-frontend/tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'wiki-race-frontend/app';
+import config from 'wiki-race-frontend/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
