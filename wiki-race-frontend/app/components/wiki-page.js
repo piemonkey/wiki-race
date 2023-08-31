7018ba24087a74be86c01e284fecdae43a0adf6e
Rich <piemonkey@protonmail.ch>
2023-08-31 15:48:01 +0200

    Refactor wiki page into a component

--- wiki-race-frontend/app/components/wiki-page.js
+++ wiki-race-frontend/app/components/wiki-page.js
@@ -1,18 +1,23 @@
-import Controller from '@ember/controller'
+import Component from '@glimmer/component'
 import { action } from '@ember/object'
 import { service } from '@ember/service'
 
-export default class PlayPageController extends Controller {
+export default class WikiPageComponent extends Component {
   @service store
   @service router
   @service wikiRace
 
+  get hasWon() {
+    console.log('links', this.args.page.links, this.args.page)
+    return this.args.game.status === 'Won'
+  }
+
   @action
   next(page) {
-    const { game } = this.model
+    const { game } = this.args
     const nextStep = this.store.createRecord('game-step', {
       page,
-      step: this.stepNum + 1,
+      step: this.args.stepNum + 1,
     })
     this.wikiRace.takeNextStep(game, nextStep)
     this.router.transitionTo('play.page', game.id, nextStep.step)
--- wiki-race-frontend/app/routes/play/page.js
+++ wiki-race-frontend/app/routes/play/page.js
@@ -4,35 +4,29 @@ import { service } from '@ember/service'
 export default class PlayPageRoute extends Route {
   @service store
 
-  setupController(controller, ...args) {
-    super.setupController(controller, ...args)
-    const stepNum = parseInt(this.paramsFor(this.routeName).step, 10)
-    controller.set('stepNum', stepNum)
-  }
-
   async model(params) {
     const stepNum = parseInt(params.step ?? 1, 10)
     const { game, steps: stepsProm } = await this.modelFor('play')
-    if (game.status === 'Ongoing') {
-      const stepsModel = await stepsProm
-      const stepsArray = [...stepsModel]
-      const pageId = stepsArray.find((step) => step.step === stepNum)?.page
-      if (pageId) {
-        const page = await this.store.findRecord('page', pageId, {
-          include: 'links',
-        })
-        return { page, game, steps: stepsModel, hasWon: false }
-      } else {
-        // TODO handle this better
-        console.error(
-          'no page id for this step number',
-          stepsArray.map((step) => [step.step, step.page]),
-          stepNum
-        )
-        return {}
-      }
+    if (game.status !== 'Ongoing') {
+      return { game, stepNum }
+    }
+
+    const stepsModel = await stepsProm
+    const stepsArray = [...stepsModel]
+    const pageId = stepsArray.find((step) => step.step === stepNum)?.page
+    if (pageId) {
+      const page = await this.store.findRecord('page', pageId, {
+        include: 'links',
+      })
+      return { page, game, stepNum }
     } else {
-      return { game, hasWon: true }
+      // TODO handle this better
+      console.error(
+        'no page id for this step number',
+        stepsArray.map((step) => [step.step, step.page]),
+        stepNum
+      )
+      return {}
     }
   }
 }
